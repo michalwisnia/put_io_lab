b991bddada6da635f022c702d1eedbab60fca6eb
mich4725 <michalmanu@gmail.com>
2020-10-31 23:46:20 +0100

    hunk3_zmiana

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -1,3 +1,4 @@
+//to jest komentarz
 public class MyClass1 {
   int x = 16;
 	int y = 2;
@@ -6,5 +7,5 @@ public class MyClass2 {
   int x = 4;
 }
 public class MyClass3 {
-  int x = 33;
+  int x = 31;
 }
